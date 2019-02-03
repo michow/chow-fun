148b2446d7255001fe27770d9ac880cd27e7033d
Michelle Chow <michow1@gmail.com>
2019-02-02 21:16:14 -0800

    new hover functionality for desktop

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -12,62 +12,36 @@
   $('.year').html(year);
 
   $( window ).resize(function() {
-    winWidth = $( window ).width();
+    winWidth = $(window).width();
   });
 
   $('.corner-curl').on('click', function() {
     $('.flip').addClass('flipped');
   });
 
-  $('.pricing').on('click', function() {
-    var
-      $pricing = $(this),
-      $entree = $pricing.parent(),
-      $preview = $entree.find('.preview'),
-      $default = $('.default'),
-      $previous = $('.preview:visible')
-    ;
+  // mobile open preview
+  $('.pricing a').on('click', function(e) {
+    if(winWidth <= breakLg) {
+      e.preventDefault();
 
-    if(winWidth > breakLg) {
-      $('.pricing').removeClass('active');
-      $pricing.addClass('active');
+      var
+        $pricing = $(this).parent(),
+        $preview = $pricing.next()
+      ;
 
-      if($('.default:visible').length) {
-        $default.fadeOut('fast', function() {
-          $preview.fadeIn('fast');
-        });
-      }
-
-      if($previous.length) {
-        $previous.fadeOut('fast', function() {
-          $preview.fadeIn('fast');
-        });
-      }
-    }
-    else {
       $pricing.addClass('active');
       $preview.slideDown('fast');
     }
   });
 
+  // mobile close preview
   $('.close').on('click', function(e) {
     e.preventDefault();
 
-    var
-      $preview = $(this).parent()
-    ;
+    var $preview = $(this).parent();
 
-    if(winWidth > breakLg) {
-      $('.pricing').removeClass('active');
-
-      $preview.fadeOut('fast', function() {
-        $('.default').fadeIn('fast');
-      });
-    }
-    else {
-      $preview.prev().removeClass('active');
-      $preview.slideUp('fast');
-    }
+    $preview.prev().removeClass('active');
+    $preview.slideUp('fast');
   });
 
 })(jQuery);
